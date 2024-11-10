9f21c3596b2bdd6628a53f53f87641e81de49848
apemon <760108+apemon@users.noreply.github.com>
2024-11-10 16:48:06 +0700

    add test

--- contracts/src/CuriaResolver.sol
+++ contracts/src/CuriaResolver.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.0;
 
 import {Test, console} from "forge-std/Test.sol";
 import {IEAS, Attestation} from "@eas/contracts/IEAS.sol";
@@ -10,13 +10,13 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 contract CuriaResolver is ICuriaResolver, Ownable, SchemaResolver {
     mapping(address => bool) public isIssuer;
 
-    constructor(IEAS eas) SchemaResolver(eas) Ownable(_msgSender()) {}
+    constructor(IEAS eas) SchemaResolver(eas) {}
 
-    modifier onlyIssuer() {
-        if (!isIssuer[_msgSender()]) {
+    function _checkIssuer(Attestation calldata attestation) internal view {
+        address attestor = attestation.attester;
+        if (!isIssuer[attestor]) {
             revert NotIssuer();
         }
-        _;
     }
 
     function setIssuer(address account, bool enable) external override onlyOwner {
@@ -24,23 +24,12 @@ contract CuriaResolver is ICuriaResolver, Ownable, SchemaResolver {
         emit IssuerUpdated(account, enable);
     }
 
-    function onAttest(Attestation calldata attestation, uint256 value)
-        internal
-        view
-        override
-        onlyIssuer
-        returns (bool)
-    {
+    function onAttest(Attestation calldata attestation, uint256 value) internal view override returns (bool) {
+        _checkIssuer(attestation);
         return true;
     }
 
-    function onRevoke(Attestation calldata attestation, uint256 value)
-        internal
-        view
-        override
-        onlyIssuer
-        returns (bool)
-    {
+    function onRevoke(Attestation calldata attestation, uint256 value) internal view override returns (bool) {
         return true;
     }
 }
--- contracts/test/CuriaResolver.t.sol
+++ contracts/test/CuriaResolver.t.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.0;
 
 import {Test, console} from "forge-std/Test.sol";
-import {CuriaResolver} from "@dynamic-attestation/src/CuriaResolver.sol";
+import {CuriaResolver, ICuriaResolver} from "@dynamic-attestation/src/CuriaResolver.sol";
 import {ISchemaResolver} from "@eas/contracts/resolver/ISchemaResolver.sol";
 import {ISchemaRegistry, SchemaRegistry} from "@eas/contracts/SchemaRegistry.sol";
-import {IEAS, EAS, AttestationRequest, AttestationRequestData} from "@eas/contracts/EAS.sol";
+import {IEAS, EAS, AttestationRequest, AttestationRequestData, Attestation, RevocationRequestData, RevocationRequest} from "@eas/contracts/EAS.sol";
+import {AccessDenied} from "@eas/contracts/Common.sol";
 
 contract CuriaResolverTest is Test {
-    ISchemaResolver public resolver;
+    CuriaResolver public resolver;
     IEAS public eas;
     ISchemaRegistry public schemaRegistry;
-    address public owner = address(0xbeef)
+    address public owner = address(0xbeef);
     address public issuer = address(0xcafe);
     address public anon = address(0xdead);
     bytes32 public schemaId;
@@ -22,10 +23,11 @@ contract CuriaResolverTest is Test {
         eas = new EAS(schemaRegistry);
         // register schema
         vm.startPrank(owner);
-        resolver = new CuriaResolverTest(eas);
+        resolver = new CuriaResolver(eas);
         resolver.setIssuer(issuer, true);
         vm.stopPrank();
         schemaId = schemaRegistry.register(schema, resolver, true);
+        console.log("eas", address(eas));
     }
 
     function testAttest() public {
@@ -37,11 +39,55 @@ contract CuriaResolverTest is Test {
             expirationTime: 0,
             value: 0
         });
-        AttestationRequest memory request = AttestationRequest({
-            schema: schemaId,
-            data: data
+        AttestationRequest memory request = AttestationRequest({schema: schemaId, data: data});
+        // begin with anon
+        vm.startPrank(anon);
+        vm.expectRevert(ICuriaResolver.NotIssuer.selector);
+        eas.attest(request);
+        vm.stopPrank();
+        // test with issuer
+        vm.startPrank(issuer);
+        bytes32 attestId = eas.attest(request);
+        vm.stopPrank();
+        Attestation memory attestation = eas.getAttestation(attestId);
+        assertEq(attestation.attester, issuer);
+        assertEq(attestation.recipient, anon);
+        assertEq(attestation.schema, schemaId);
+    }
+
+    function testRevoke() public {
+        AttestationRequestData memory data = AttestationRequestData({
+            recipient: anon,
+            revocable: true,
+            refUID: bytes32(0),
+            data: abi.encodeWithSignature(schema, "top100", true, "2024-11-09"),
+            expirationTime: 0,
+            value: 0
         });
-        bytes32 attest = eas.attest(request);
-        console.log(vm.toString(attest));
+        AttestationRequest memory request = AttestationRequest({schema: schemaId, data: data});
+        // test with issuer
+        vm.startPrank(issuer);
+        bytes32 attestId = eas.attest(request);
+        vm.stopPrank();
+        // test revoke with anon
+        RevocationRequestData memory revocationData = RevocationRequestData({
+          uid: attestId,
+          value: 0
+        });
+        RevocationRequest memory revocation = RevocationRequest({
+          schema: schemaId,
+          data: revocationData
+        });
+
+        vm.startPrank(anon);
+        vm.expectRevert(AccessDenied.selector);
+        eas.revoke(revocation);
+        vm.stopPrank();
+
+        vm.startPrank(issuer);
+        eas.revoke(revocation);
+        vm.stopPrank();
+        Attestation memory attestation = eas.getAttestation(attestId);
+        assertEq(attestation.revocationTime, block.timestamp);
     }
 }
