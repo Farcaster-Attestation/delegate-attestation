712d0b63ccf27917e2b981c2bbcd9a82eeb261cf
Chomtana <Chomtana001@gmail.com>
2025-06-16 02:00:49 +0700

    Update readme

--- attester-agora/src/main.ts
+++ attester-agora/src/main.ts
@@ -11,46 +11,48 @@ import { xor } from "lodash";
 let running = false;
 
 export async function main() {
-  if (running) {
-    return;
-  }
-  running = true;
-
-  logger.info("Updating ranker attestation...");
-
-  const ranks = await getRanks();
-  const topDelegators = await fetchAgoraTopDelegators();
-  const topDelegatorsAddresses = topDelegators
-    .map((d) => d.address)
-    .map((a) => getAddress(a));
-
-  const needUpdates = xor(ranks, topDelegatorsAddresses);
-
-  if (needUpdates.length > 0) {
-    logger.info(`Need to update ${needUpdates.length} ranks`);
-
-    for (const delegator of needUpdates) {
-      logger.info(`Updating ${delegator} rank`);
-      const delegations = await fetchAdvancedDelegations(delegator);
-
-      if (delegations.length > 0) {
-        logger.info(
-          `Updating ${delegator} subdelegations from ${delegations
-            .map((d) => d.from)
-            .join(", ")}`
-        );
-
-        // Update sub delegation rules
-        await updateMultipleSubDelegationRules(
-          delegations.map((d) => d.from),
-          delegations.map((d) => d.to)
-        );
-      }
-
-      // Update rank
-      await updateRank(delegator);
+  try {
+    if (running) {
+      return;
     }
-  }
+    running = true;
 
-  running = false;
+    logger.info("Updating ranker attestation...");
+
+    const ranks = await getRanks();
+    const topDelegators = await fetchAgoraTopDelegators();
+    const topDelegatorsAddresses = topDelegators
+      .map((d) => d.address)
+      .map((a) => getAddress(a));
+
+    const needUpdates = xor(ranks, topDelegatorsAddresses);
+
+    if (needUpdates.length > 0) {
+      logger.info(`Need to update ${needUpdates.length} ranks`);
+
+      for (const delegator of needUpdates) {
+        logger.info(`Updating ${delegator} rank`);
+        const delegations = await fetchAdvancedDelegations(delegator);
+
+        if (delegations.length > 0) {
+          logger.info(
+            `Updating ${delegator} subdelegations from ${delegations
+              .map((d) => d.from)
+              .join(", ")}`
+          );
+
+          // Update sub delegation rules
+          await updateMultipleSubDelegationRules(
+            delegations.map((d) => d.from),
+            delegations.map((d) => d.to)
+          );
+        }
+
+        // Update rank
+        await updateRank(delegator);
+      }
+    }
+  } finally {
+    running = false;
+  }
 }
